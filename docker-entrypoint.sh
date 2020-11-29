d643605c6dfae1f9acb6c3a1c6474cd301fcd013
Darebeat <fengwei2010@126.com>
2020-11-29 22:55:47 +0800

    adj: tab to blank adjust

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,34 +17,34 @@ usage() {
 }
 
 h2(){
-	java -jar /app/mblog/mblog-latest.jar
+  java -jar /app/mblog/mblog-latest.jar
 }
 
 mysql(){
-	spring_datasource_url=${spring_datasource_url:-"jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?useSSL=false&characterEncoding=utf8&serverTimezone=GMT%2B8"}
-	spring_datasource_username=${spring_datasource_username:-"${MYSQL_USERNAME}"}
-	spring_datasource_password=${spring_datasource_password:-"${MYSQL_PASSWORD}"}
-	
-	java -jar /app/mblog/mblog-latest.jar \
+  spring_datasource_url=${spring_datasource_url:-"jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?useSSL=false&characterEncoding=utf8&serverTimezone=GMT%2B8"}
+  spring_datasource_username=${spring_datasource_username:-"${MYSQL_USERNAME}"}
+  spring_datasource_password=${spring_datasource_password:-"${MYSQL_PASSWORD}"}
+  
+  java -jar /app/mblog/mblog-latest.jar \
     -Dspring.datasource.url=${spring_datasource_url} \
     -Dspring.datasource.username=${spring_datasource_username} \
     -Dspring.datasource.password=${spring_datasource_password}
 }
 
 case "$1" in
-	"h2")
-		h2
-		;;
-	"mysql")
-		mysql
-		;;
-	"--help")
-		usage
-		;;
-	"-h")
-		usage
-		;;
-	*)
+  "h2")
+    h2
+    ;;
+  "mysql")
+    mysql
+    ;;
+  "--help")
+    usage
+    ;;
+  "-h")
+    usage
+    ;;
+  *)
     echo "It's not a valid param: $@"
     usage
     ;;
